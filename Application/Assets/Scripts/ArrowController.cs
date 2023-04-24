d6a09c84732b2c3edbe96c4f2e0e1bd08b564890
s-n-1-0 <72431055+s-n-1-0@users.noreply.github.com>
2023-04-24 16:21:33 +0900

    矢印コントローラーを追加

---
+++ Application/Assets/Scripts/ArrowController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArrowController : MonoBehaviour
+{
+    //public bool isLeft = true;
+    public float shiftSpeed = 0.1f;
+    private MeshRenderer mesh;
+    void Start()
+    {
+        mesh = GetComponent<MeshRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        mesh.material.SetFloat("_Speed", shiftSpeed);   
+    }
+}
