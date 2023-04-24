ae331d47d971cebf6a74a7bac8f7e98dbf08daf1
s-n-1-0 <72431055+s-n-1-0@users.noreply.github.com>
2023-04-24 16:56:56 +0900

    ArrowControllerにメッシュ切り替えオプションを追加

--- Application/Assets/Scripts/ArrowController.cs
+++ Application/Assets/Scripts/ArrowController.cs
@@ -1,12 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering;
 
 public class ArrowController : MonoBehaviour
 {
     public bool isArrowLeft = true;
     public bool isStripeSameDirectionAsArrow = true;
     public float shiftSpeed = 0.1f;
+    public ArrowType arrowType = ArrowType.Stripe;
+    public Material  defaultMat, stripeMat;
     private MeshRenderer mesh;
     void Start()
     {
@@ -16,7 +19,16 @@ public class ArrowController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        mesh.material.SetFloat("_Speed", Mathf.Abs(shiftSpeed) * (isStripeSameDirectionAsArrow ? 1:-1));
-        transform.eulerAngles = new Vector3(90f + (isArrowLeft ? 0f : 180f),90f,90f);
+         mesh.material.SetFloat("_Speed", Mathf.Abs(shiftSpeed) * (isStripeSameDirectionAsArrow ? 1 : -1));
+        transform.eulerAngles = new Vector3(90f + (isArrowLeft ? 0f : 180f), 90f, 90f);
+    }
+    private void OnValidate()
+    {
+        if (mesh)  mesh.material = arrowType == ArrowType.Default ? defaultMat : stripeMat;
     }
 }
+public enum ArrowType {
+    Default,
+    Stripe
+}
+
