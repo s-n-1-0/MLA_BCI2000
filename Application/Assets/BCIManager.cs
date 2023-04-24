d3d62b5193040221307163b38d6b7fa932612a66
s-n-1-0 <72431055+s-n-1-0@users.noreply.github.com>
2023-04-24 17:23:26 +0900

    BCI管理クラスの追加

---
+++ Application/Assets/BCIManager.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BCIManager : CustomVariableBase
+{
+    public int yClass;
+    public ArrowController arrowController;
+    public override void AddCustomVariables()
+    {
+        //TODO: 適切な変数指定
+        customVariables.Add(new CustomGetVariable(
+            "YClass",
+            new Action<int>((int i) => { yClass = i; })
+        ));
+    }
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //TODO: クラス分類結果に応じた矢印設定
+        switch (yClass)
+        {
+            case 0:
+                break;
+        }
+    }
+}
