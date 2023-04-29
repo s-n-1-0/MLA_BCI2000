e1d926d53916ddfcb302bbe813929936dd0671be
s-n-1-0 <72431055+s-n-1-0@users.noreply.github.com>
2023-04-29 23:07:05 +0900

    アプリデモ用シーンの追加

---
+++ Application/Assets/Scripts/Demo/DemoAppManager.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DemoAppManager : MonoBehaviour
+{
+    private ArrowController arrow;
+    public int trialsNum = 30;
+    public TaskState state { get; } = new TaskState();
+    private void Start()
+    {
+        var arrowObj = GameObject.Find("Target");
+        arrow = arrowObj.GetComponent<ArrowController>();
+        arrow.gameObject.SetActive(false);
+        StartTask();
+    }
+
+    private void Update()
+    {
+        if (state.trueClassType != TrialClassType.None)
+        {
+            arrow.SetArrowType(state.isFeedback && state.predictClassType != TrialClassType.None ? ArrowType.Stripe : ArrowType.Default); //FB有でpredictの結果が返されているなら
+            arrow.isArrowLeft = state.trueClassType == TrialClassType.Left;
+            arrow.isStripeSameDirectionAsArrow = state.trueClassType == state.predictClassType;
+        }
+        else
+        {
+            arrow.SetArrowType(ArrowType.Default);
+        }
+    }
+
+    public void StartTask()
+    {
+        StartCoroutine(_StartTask());
+    }
+    private IEnumerator _StartTask()
+    {
+        //---試行
+        while (true)
+        {
+            var trueClass = (TrialClassType)(1 + Random.Range(0, 2));
+            ChangeWaitScreen();
+            yield return new WaitForSeconds(1f);
+            state.trueClassType = trueClass;
+            ChangeTargetScreen();
+            yield return new WaitForSecondsRealtime(5f);
+
+        }
+    }
+    private void ChangeWaitScreen()
+    {
+        arrow.gameObject.SetActive(false);
+    }
+    private void ChangeTargetScreen()
+    {
+        arrow.gameObject.SetActive(true);
+    }
+}
---
+++ Application/Assets/Scripts/Demo/DemoAppUIController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DemoAppUIController : MonoBehaviour
+{
+    private TaskState state;
+    void Start()
+    {
+        state = gameObject.GetComponent<DemoAppManager>().state;
+    }
+
+    public void ChangeTargetDirection(bool isLeft)
+    {
+        state.predictClassType = isLeft ? TrialClassType.Left : TrialClassType.Right;
+    }
+}
