d0deb18cbb0ca377a5f010e8cfc1647548e91841
s-n-1-0 <72431055+s-n-1-0@users.noreply.github.com>
2023-04-29 22:04:28 +0900

    矢印4種類のデモを管理するコンポーネントを追加

---
+++ Application/Assets/Scripts/Demo/Demo4PatternsManager.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Demo4PatternsManager : MonoBehaviour
+{
+    public ArrowController arrow;
+    private void Start()
+    {
+        var arrowObj = GameObject.Find("Target");
+        arrow = arrowObj.GetComponent<ArrowController>();
+        arrow.gameObject.SetActive(false);
+        StartTask();
+    }
+
+    public void StartTask()
+    {
+        StartCoroutine(_StartTask());
+    }
+    private IEnumerator _StartTask()
+    {
+        //---セットアップ
+        var trials = new TrialClassType[] { TrialClassType.Right,TrialClassType.Right,TrialClassType.Left,TrialClassType.Left};
+        var predicts = new TrialClassType[] { TrialClassType.Right,TrialClassType.Left,TrialClassType.Right,TrialClassType.Left };
+        arrow.SetArrowType(ArrowType.Stripe);
+        //---試行
+        for (int i = 0; i < trials.Length; i++)
+        {
+            ChangeWaitScreen();
+            yield return new WaitForSeconds(1f);
+            ChangeTargetScreen(trials[i], predicts[i]);
+            yield return new WaitForSecondsRealtime(5f);
+        }
+        ChangeWaitScreen();
+    }
+    private void ChangeWaitScreen()
+    {
+        arrow.gameObject.SetActive(false);
+    }
+    private void ChangeTargetScreen(TrialClassType trueType,TrialClassType predictType)
+    {
+        arrow.isArrowLeft = trueType == TrialClassType.Left;
+        arrow.isStripeSameDirectionAsArrow = trueType == predictType;
+        arrow.gameObject.SetActive(true);
+    }
+}
