87c5c7b5e6231339bce5f6d0c359b25a6d4c783e
s-n-1-0 <72431055+s-n-1-0@users.noreply.github.com>
2023-05-12 22:50:34 +0900

    BCI2000の設定をjson形式で行えるように対応

--- Application/Assets/Scripts/BCIManager.cs
+++ Application/Assets/Scripts/BCIManager.cs
@@ -1,10 +1,25 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
 
 public class BCIManager : CustomVariableBase
 {
+    private void Awake()
+    {
+        string filePath = Application.streamingAssetsPath + "/BCISettings.json";
+        StreamReader reader = new StreamReader(filePath);
+        string json = reader.ReadToEnd();
+        reader.Close();
+        var bciSettings = JsonUtility.FromJson<BCI2000SettingsJson>(json);
+        var bci = GetComponent<UnityBCI2000>();
+        bci.OperatorPath = bciSettings.operatorPath;
+        bci.Module1 = bciSettings.module1Name;
+        bci.Module2 = bciSettings.module2Name;
+        bci.Module3 = bciSettings.module3Name;
+        bci.enabled = true;
+    }
     public override void AddCustomVariables()
     {
         var state = GetComponent<TaskManager>().state;
---
+++ Application/Assets/Scripts/Models/json/BCI2000SettingsJson.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class BCI2000SettingsJson
+{
+    public string operatorPath,module1Name,module2Name,module3Name;
+}
