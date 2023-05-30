c04e4c53b9847e90e6bfbb7808916acaf0e24c37
s-n-1-0 <72431055+s-n-1-0@users.noreply.github.com>
2023-05-30 15:18:53 +0900

    UIキャンバスに試行番目を表示

--- Application/Assets/UICanvasController.cs
+++ Application/Assets/UICanvasController.cs
@@ -1,9 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class UICanvasController : MonoBehaviour
 {
     public TaskManager task;
+    public TextMeshProUGUI text;
+    private void Update()
+    {
+        text.text = task.state.trialNum.ToString();
+    }
     public void clickNFBButton(bool isNFB) => task.StartTask(isNFB);
 }
