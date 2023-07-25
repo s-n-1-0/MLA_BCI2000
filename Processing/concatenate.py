0bd7e8792657c630522f750d709ad4b9b80beb4f
s-n-1-0 <72431055+s-n-1-0@users.noreply.github.com>
2023-07-25 15:39:17 +0900

    連結処理の取得範囲が前からなので後ろからに修正

--- Processing/concatenate.py
+++ Processing/concatenate.py
@@ -9,10 +9,9 @@ data_files = [d for d in os.listdir() if ".npy" == d[-4:]]
 data_list = []
 for df in data_files:
     data = np.load(df)
-    print(data.shape)
     data_list.append(data)
 
 min_size = np.min([d.shape[1] for d in data_list])
-resized_data = np.array([d[:,:min_size] for d in data_list])
+resized_data = np.array([d[:,-min_size:] for d in data_list])
 current_directory = os.path.basename(os.getcwd())
 np.save(f"../{current_directory}_concatenate",resized_data)
