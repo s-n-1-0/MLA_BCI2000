8de9a3cd0ca77c4dbfab48e39c7830ff34cc8fb5
s-n-1-0 <72431055+s-n-1-0@users.noreply.github.com>
2023-08-06 23:45:55 +0900

    デコーダ1・2のモデルカードを生成

---
+++ Analytics/model_card/model_card_template.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python
+# coding: utf-8
+# %%
+import model_card_toolkit as mct
+import base64
+import pandas as pd
+import matplotlib.pyplot as plt
+from io import BytesIO
+import utils
+#学習履歴の取得
+history = pd.read_csv("c:/lab/models/dec1/history.csv")
+history
+# %%
+epochs = len(history["binary_accuracy"])
+history_base64_list = []
+for item in ["binary_accuracy","loss","recall","specificity"]:
+    plt.plot(range(1,epochs+1),history[item],label="train_"+item)
+    plt.plot(range(1,epochs+1),history["val_"+item],label="valid_"+item)
+    plt.legend(loc="upper left",bbox_to_anchor=(1,1))
+    img_in_memory = BytesIO()
+    plt.savefig(img_in_memory,format='png', bbox_inches='tight')
+    img_in_memory.seek(0)
+    plt.show()
+    history_base64_list.append(base64.b64encode(img_in_memory.getvalue()))
+# %%
+#　概要
+#
+toolkit,model_card = utils.build_base_card('Decoder 1号',"001")
+details = model_card.model_details
+details.version.diff="初期バージョン"
+details.overview = (
+    "EEGNetを使用し、マルチチャネルEEGデータ(左手・右手の運動イメージ)を2値分類します。\n"
+    "学習データにはOpenBMIによって測定された54人の公開データを使用します。")
+details.documentation = ("前処理としてEEGLABを通し、アーチファクトの影響の多いデータの除去、ダウンサンプリング(1000hz->500hz)、バンドパスフィルターの適用(1hzから30hz)を行っています。\n"
+                         "EEGLABを通した後、標準化とバンドパスフィルタ(8hzから30hz)を適用しています。\n"
+                         "['FC5','FC1','FC2','FC6','C3','C1','Cz','C2','C4','CP5','CP1','CP2','CP6']電極(fs=500hz)を使用。[54, 34, 21, 35, 1]番の被験者を検証データ、それ以外を学習データにしました。")
+#
+#技術的パラメータ
+#
+#データセットについて
+parameters = model_card.model_parameters
+parameters.model_architecture="EEGNet"
+parameters.input_format= "生波形を入力しています。形式は3次元テンソルです。750サンプルはランダム区間で選択"
+parameters.input_format_map = [mct.KeyVal("1","16ch"),
+                               mct.KeyVal("2","750 samples"),
+                               mct.KeyVal("3","1")]
+parameters.output_format="シグモイドによる2値(左:0,右:1)出力"
+
+#
+#評価
+#
+model_card.quantitative_analysis.graphics.description = "正解率(Accuracy),損失(Loss),再現率(Recall),特異度(Specificity)"
+model_card.quantitative_analysis.graphics.collection =[mct.Graphic("",h64) for h64 in history_base64_list]
+
+# %%
+utils.save_and_display("test.html",toolkit,model_card)
+
---
+++ Analytics/model_card/utils/base_card.py
@@ -0,0 +1,43 @@
+#
+# TODO: 今後データセット等を追加する場合、コードを分割すること
+#
+import model_card_toolkit as mct
+import datetime
+import requests
+import base64
+from IPython.display import HTML
+# 画像データの取得
+response = requests.get("https://i.gyazo.com/dc04eaf10fa1e527b567261dd516d0e9.png")
+file_data = response.content
+openbmi_mitask_image = base64.b64encode(file_data).decode('utf-8')
+
+def build_base_card(model_name:str,
+                    version:str):
+    toolkit = mct.ModelCardToolkit()
+    model_card = toolkit.scaffold_assets()
+    details = model_card.model_details
+    details.name = model_name
+    details.owners = [
+    mct.Owner(name = 'BCI研究グループ', contact= '静岡理工科大学大学院')
+    ]
+    details.references = [mct.Reference(
+    'https://iopscience.iop.org/article/10.1088/1741-2552/aace8c/meta'
+    ),mct.Reference(
+        'https://academic.oup.com/gigascience/article/8/5/giz002/5304369'
+    )]
+    v = details.version
+    v.name = version
+    v.date = str(datetime.datetime.now())
+    model_card.model_parameters.data = [mct.Dataset(name="OpenBMIデータセット(3パラダイム)",
+                    description="ERP・MI・SSVEPのタスクを行った54名の被験者データ(MIデータのみ使用)",
+                    graphics=mct.GraphicsCollection(("MIタスク図"),[mct.Graphic("",openbmi_mitask_image)]))]
+    
+    return toolkit,model_card
+
+
+def save_and_display(ex_path:str,toolkit,card):
+    toolkit.update_model_card(card)
+    html = toolkit.export_format()
+    with open(ex_path, "w") as f:
+        f.write(html)
+    return HTML(html)
