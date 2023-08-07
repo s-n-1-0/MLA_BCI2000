7bb4bfada9386d3333a955c567a00523fb0ce552
s-n-1-0 <72431055+s-n-1-0@users.noreply.github.com>
2023-08-07 15:44:19 +0900

    実験の注意点説明を追加

--- Application/Assets/UICanvasController.cs
+++ Application/Assets/UICanvasController.cs
@@ -7,12 +7,13 @@ public class UICanvasController : MonoBehaviour
 {
     public TaskManager task;
     public TextMeshProUGUI trialsNumText,maxTrialsNumText,stimTimeText;
-
+    public GameObject announcePanel;
     private void Update()
     {
         trialsNumText.text = task.state.trialNum.ToString();
     }
-    public void clickNFBButton(bool isNFB) => task.StartTask(isNFB);
+    public void ToggleAnnouncePanel() => announcePanel.SetActive(!announcePanel.activeSelf);
+    public void ClickNFBButton(bool isNFB) => task.StartTask(isNFB);
     public void SetStimTIme(float time) => stimTimeText.text = $"Stim : {time}s";
     public void SetMaxTrialsNum(int num) =>maxTrialsNumText.text = $"Max : {num}" ;
     
