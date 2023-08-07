d0e042eead33b3e04cad1099ac57c30f212bd910
s-n-1-0 <72431055+s-n-1-0@users.noreply.github.com>
2023-08-07 16:17:57 +0900

    課題開始前に操作説明を行うように対応

--- Application/Assets/UICanvasController.cs
+++ Application/Assets/UICanvasController.cs
@@ -6,14 +6,21 @@ using UnityEngine;
 public class UICanvasController : MonoBehaviour
 {
     public TaskManager task;
-    public TextMeshProUGUI trialsNumText,maxTrialsNumText,stimTimeText;
-    public GameObject announcePanel;
+    public TextMeshProUGUI trialsNumText, maxTrialsNumText, stimTimeText;
+    public GameObject announcePanel, testPanel, trainPanel;
     private void Update()
     {
         trialsNumText.text = task.state.trialNum.ToString();
     }
     public void ToggleAnnouncePanel() => announcePanel.SetActive(!announcePanel.activeSelf);
-    public void ClickNFBButton(bool isNFB) => task.StartTask(isNFB);
+    public void ToggleTestPanel() => testPanel.SetActive(!testPanel.activeSelf);
+    public void ToggleTrainPanel() => trainPanel.SetActive(!trainPanel.activeSelf);
+    public void ClickNFBButton(bool isNFB)
+    {
+        testPanel.SetActive(false);
+        trainPanel.SetActive(false);
+        task.StartTask(isNFB);
+    }
     public void SetStimTIme(float time) => stimTimeText.text = $"Stim : {time}s";
     public void SetMaxTrialsNum(int num) =>maxTrialsNumText.text = $"Max : {num}" ;
     
