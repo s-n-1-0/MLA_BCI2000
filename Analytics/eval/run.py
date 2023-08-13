90e0e80fb01a55378352aa50f2e94176ca1795c9
s-n-1-0 <72431055+s-n-1-0@users.noreply.github.com>
2023-08-13 18:14:12 +0900

    評価処理の設定に〇日目オプションを追加
    (cherry picked from commit ccf83568dd72ad85bd85d3cf7646b8c4cd663032)

--- Analytics/eval/run.py
+++ Analytics/eval/run.py
@@ -6,15 +6,16 @@ import json
 with open('settings.json') as f:
     settings = json.load(f)
 subject = settings["subject"]
-subject_dir = f"{settings['dataset_dir']}/{subject}"
+day = settings["day"]
+subject_day_dir = f"{settings['dataset_dir']}/{subject}/{day}"
 # %%
-file_paths = [f for f in glob.glob(subject_dir + "/*") if f.endswith(".npy") ]
+file_paths = [f for f in glob.glob(subject_day_dir + "/*") if f.endswith(".npy") ]
 file_names = [os.path.split(f)[1][:-4] for f in file_paths]
 file_names
 # %%
 for data_path,name in zip(file_paths,file_names):
     name = name.replace("_concatenate","")
-    eval_dir = f"{settings['dataset_dir']}/evals/{subject}"
+    eval_dir = f"{settings['dataset_dir']}/evals/{subject}/{day}"
     if not os.path.exists(eval_dir):
         os.makedirs(eval_dir)
     pm.execute_notebook(
