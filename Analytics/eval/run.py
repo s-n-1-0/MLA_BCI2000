320e2f76cbb47f61d65835eadb9a18031a0c71e5
s-n-1-0 <72431055+s-n-1-0@users.noreply.github.com>
2023-08-14 16:36:01 +0900

    papermillにモデルのパス指定

--- Analytics/eval/run.py
+++ Analytics/eval/run.py
@@ -13,6 +13,8 @@ file_paths = [f for f in glob.glob(subject_day_dir + "/*") if f.endswith(".npy")
 file_names = [os.path.split(f)[1][:-4] for f in file_paths]
 file_names
 # %%
+d1_model_path = "C:/MLA Models/d1_model.h5"
+d2_model_path = "C:/MLA Models/d2_model.h5"
 for data_path,name in zip(file_paths,file_names):
     name = name.replace("_concatenate","")
     eval_dir = f"{settings['dataset_dir']}/evals/{subject}/{day}"
@@ -21,5 +23,5 @@ for data_path,name in zip(file_paths,file_names):
     pm.execute_notebook(
     "eval.ipynb",
     f"{eval_dir}/{name}.ipynb",
-    parameters=dict(data_path=data_path)
+    parameters=dict(data_path=data_path,d1_model_path=d1_model_path,d2_model_path=d2_model_path)
     )
