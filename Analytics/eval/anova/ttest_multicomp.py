ec700da5c3f5248f58e3cfaf7874ada578724f49
s-n-1-0 <72431055+s-n-1-0@users.noreply.github.com>
2023-12-28 16:20:43 +0900

    多重比較を追加

---
+++ Analytics/eval/anova/ttest_multicomp.py
@@ -0,0 +1,33 @@
+# %%
+import pingouin as pg
+import pandas as pd
+import sys
+sys.path.append('../')
+from load_excel import get_data_from_excel
+# %%
+excel_path = "C:/Users/2221012/Downloads/1+2日目.xlsx"
+subjects = 15
+a_groups = [3,5,8,10,11,12,16,17]
+b_groups = [1,2,4,7,9,13,15]
+subjects = 15
+pd.options.display.precision = 7
+def read_data():
+    return get_data_from_excel(excel_path,subjects=subjects,iloc=17,index_col=0)
+read_data()
+# %%
+data = read_data()
+a_data = data.loc[a_groups]
+b_data = data.loc[b_groups]
+def ttest(i:int):
+    a = a_data.values[:,i]
+    b = b_data.values[:,i]
+    return pg.ttest(a, b, correction=False)
+results = pd.concat([ttest(i) for i in range(16)])
+results.insert(3,"p-val(FDR/bh)",None)
+results
+# %%
+fdr = pg.multicomp(results["p-val"].values, method='fdr_bh') 
+fdr
+# %%
+results["p-val(FDR/bh)"] = fdr[1]
+results
