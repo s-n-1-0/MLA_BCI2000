dd310cb9ec36b902b3670c51ebabadbf253733ff
s-n-1-0 <72431055+s-n-1-0@users.noreply.github.com>
2024-01-25 11:19:46 +0900

    全員のrmanovaオプションの追加

--- Analytics/eval/test/rm_anova.py
+++ Analytics/eval/test/rm_anova.py
@@ -10,7 +10,7 @@ from load_excel import get_data_from_excel
 # %%
 excel_path = "C:/Users/2221012/Downloads/1+2日目.xlsx"
 subjects = 15
-is_a = True
+gorup_mode = True #A:True,B:False,全員:None
 a_group = [3,5,8,10,11,12,16,17]
 b_group = [1,2,4,7,9,13,15]
 def read_data():
@@ -20,11 +20,14 @@ read_data()
 _df = read_data()
 df = pd.DataFrame(columns = ["Subject","Time","Scores"])
 for time in _df.columns:
-    for s in (a_group if is_a else b_group):
+    group = a_group + b_group
+    if gorup_mode is not None: group = (a_group if gorup_mode else b_group)
+    for s in group:
         df.loc[len(df)] = [s, time,_df.loc[s,time]]
 df
 # %% 1要因 rm分散分析
-print("A" if is_a else "B")
+if gorup_mode is None : print("全員")
+else: print("A" if gorup_mode else "B")
 aov = pg.rm_anova(data=df, dv='Scores', within='Time', subject='Subject', detailed=True,effsize="n2")
 aov
 
